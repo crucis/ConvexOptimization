3e91a77dad87a776cac0e7104781847a33f55cae
Pedro Bandeira de Mello Martins <bandeira85@estinge17.pads.ufrj.br>
2018-10-05 18:57:55 -0300

    First optimization algorithm done, added epsilon to init.

--- models/optimizers/DichotomousSearch.py
+++ models/optimizers/DichotomousSearch.py
@@ -1,22 +1,29 @@
-from optimizer import optimizer
-import autograd.numpy as np
+from .optimizer import optimizer
+import numpy as np
 
 class DichotomousSearch(optimizer):
+    def __init__(self, func, epsilon, maxIter = 1e18, interval = [-1e6, 1e6], xtol = 1e-6, ftol = 1e-6):
+         self.epsilon = epsilon
+         super().__init__(func, maxIter, interval, xtol, ftol)
+
+
     def find_min(self):
-        for iteration in self.maxIter:
+        for iteration in range(self.maxIter):
             self.x_1 = self._get_middle_interval()
-            self.x_a = self.x_1 - self.xtol/2
-            self.x_b = self.x_1 + self.xtol/2
+            self.x_a = self.x_1 - self.epsilon
+            self.x_b = self.x_1 + self.epsilon
             self.fx_a = self.objectiveFunction(self.x_a)
             self.fx_b = self.objectiveFunction(self.x_b)
             self._get_new_interval()
-            if self.interval[1] - self.interval[0] == self.ftol:
+            if (self.interval[1] - self.interval[0]) <= self.xtol:
                 break
+        return self._get_middle_interval()
 
 
     def _get_middle_interval(self):
-        return (self.interval[1] - self.interval[0])/2
+        return (self.interval[1] + self.interval[0])/2
     
+
     def _get_new_interval(self):
         if self.fx_a < self.fx_b:
             self.interval[1] = self.x_b
