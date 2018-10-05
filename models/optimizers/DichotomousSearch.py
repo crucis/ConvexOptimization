aeea6bf74577927914f89362f8961b473dc46dda
Pedro Bandeira de Mello Martins <bandeira85@estinge17.pads.ufrj.br>
2018-10-05 18:19:03 -0300

    Did first version of DichotomousSearch.

--- models/optimizers/DichotomousSearch.py
+++ models/optimizers/DichotomousSearch.py
@@ -1,2 +1,28 @@
-class DichotomousSearch:
-    def __init__(func, interval = (-1e6, 1e6)):
+from optimizer import optimizer
+import autograd.numpy as np
+
+class DichotomousSearch(optimizer):
+    def find_min(self):
+        for iteration in self.maxIter:
+            self.x_1 = self._get_middle_interval()
+            self.x_a = self.x_1 - self.xtol/2
+            self.x_b = self.x_1 + self.xtol/2
+            self.fx_a = self.objectiveFunction(self.x_a)
+            self.fx_b = self.objectiveFunction(self.x_b)
+            self._get_new_interval()
+            if self.interval[1] - self.interval[0] == self.ftol:
+                break
+
+
+    def _get_middle_interval(self):
+        return (self.interval[1] - self.interval[0])/2
+    
+    def _get_new_interval(self):
+        if self.fx_a < self.fx_b:
+            self.interval[1] = self.x_b
+        elif self.fx_a > self.fx_b:
+            self.interval[0] = self.x_a
+        else:
+            self.interval[1] = self.x_b
+            self.interval[0] = self.x_a
+
