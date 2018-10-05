fc2ecfb17604035aad80b5573ba03397f7914e75
Pedro Bandeira de Mello Martins <bandeira85@estinge17.pads.ufrj.br>
2018-10-05 18:59:06 -0300

    Forced maxIter to be int

--- models/optimizers/optimizer.py
+++ models/optimizers/optimizer.py
@@ -1,7 +1,7 @@
 class optimizer:
-    def __init__(self, func, maxIter = 100, interval = (-1e6, 1e6), xtol = 1e-6, ftol = 1e-6):
+    def __init__(self, func, maxIter = 1e18, interval = [-1e6, 1e6], xtol = 1e-6, ftol = 1e-6):
         self.objectiveFunction = func
-        self.maxIter = maxIter
+        self.maxIter = int(maxIter)
         self.interval = interval
         self.xtol = xtol
         self.ftol = ftol
