b6abf1a94188a6702f93b92b25967d0b460e78e9
Pedro Bandeira de Mello Martins <bandeira85@estinge17.pads.ufrj.br>
2018-10-05 18:17:23 -0300

    Added self to class methdos.

--- models/functionObj.py
+++ models/functionObj.py
@@ -1,17 +1,16 @@
 import autograd.numpy as np
 
 class functionObj:
-    def __init__(func, domain = (-1e28, 1e28)):    
+    def __init__(self, func):    
         self.func = func
-        self.domain = domain
         self.fevals = 0
         self.best_x = np.inf
 
-    def __call__(x):
+    def __call__(self, x):
         self.fevals = self.fevasl + 1
         result = self.func(x)
         assert np.isnan(result) == False, "X out of domain"
         return result
 
-    def reset_fevals():
+    def reset_fevals(self):
         self.fevals = 0
