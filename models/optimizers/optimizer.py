f0902e61ec284cfcc87a730d6e64f8da4e777e5b
Pedro Bandeira de Mello Martins <bandeira85@estinge17.pads.ufrj.br>
2018-10-05 18:18:39 -0300

    Created first method for optimizer (find_min) and deleted domain param.

--- models/optimizers/optimizer.py
+++ models/optimizers/optimizer.py
@@ -1,2 +1,10 @@
 class optimizer:
-    def __init__():
+    def __init__(self, func, maxIter = 100, interval = (-1e6, 1e6), xtol = 1e-6, ftol = 1e-6):
+        self.objectiveFunction = func
+        self.maxIter = maxIter
+        self.interval = interval
+        self.xtol = xtol
+        self.ftol = ftol
+    
+    def find_min(self):
+        pass
