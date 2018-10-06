e033e751b3c6d6275ae3af4de7fb5d8dea61f5e3
Pedro Bandeira <bandeira85@gmail.com>
2018-10-06 01:14:01 -0300

    Added cubic interpolation to module.

--- models/optimizers/__init__.py
+++ models/optimizers/__init__.py
@@ -2,6 +2,13 @@ from .DichotomousSearch import DichotomousSearch
 from .FibonacciSearch import FibonacciSearch
 from .GoldenSectionSearch import GoldenSectionSearch
 from .QuadraticInterpolationSearch import QuadraticInterpolationSearch
+from .CubicInterpolation import CubicInterpolation
 from .optimizer import optimizer
 
-__all__ = ['DichotomousSearch', 'FibonacciSearch', 'GoldenSectionSearch', 'QuadraticInterpolationSearch', 'optimizer']
+__all__ = [
+    'DichotomousSearch', 
+    'FibonacciSearch', 
+    'GoldenSectionSearch', 
+    'QuadraticInterpolationSearch', 
+    'CubicInterpolation',
+    'optimizer']
