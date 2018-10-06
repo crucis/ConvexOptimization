f4b4f8f06a1ae804316eb469bcb09a5c079edb9b
Pedro Bandeira <bandeira85@gmail.com>
2018-10-05 22:34:51 -0300

    Added GoldenSectionSearch to module.

--- models/optimizers/__init__.py
+++ models/optimizers/__init__.py
@@ -1,5 +1,6 @@
 from .DichotomousSearch import DichotomousSearch
 from .FibonacciSearch import FibonacciSearch
+from .GoldenSectionSearch import GoldenSectionSearch
 from .optimizer import optimizer
 
-__all__ = ['DichotomousSearch', 'FibonacciSearch', 'optimizer']
+__all__ = ['DichotomousSearch', 'FibonacciSearch', 'GoldenSectionSearch', 'optimizer']
