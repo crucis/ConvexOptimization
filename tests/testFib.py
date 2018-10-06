c2a7c00243f83e1601d72b913a3c0ad25582789a
Pedro Bandeira <bandeira85@gmail.com>
2018-10-05 22:13:51 -0300

    Deleted unused parameter epsilon.

--- tests/testFib.py
+++ tests/testFib.py
@@ -4,7 +4,7 @@ from models.optimizers import FibonacciSearch
 f_x = lambda x: x**2 - 4*x + 4
 f_x_obj = functionObj(f_x)
 
-opt = FibonacciSearch(f_x_obj, epsilon = 1e-9, xtol = 1e-6, maxIter=1e6)
+opt = FibonacciSearch(f_x_obj, xtol = 1e-6, maxIter=2e10)
 
 print(opt.find_min())
 print(f_x_obj.fevals)
