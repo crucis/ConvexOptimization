7b3b892fed65c6a52e1d4083eaa0e0b099db0160
Pedro Bandeira <bandeira85@gmail.com>
2018-10-05 22:14:52 -0300

    Fixed x_b initialization, used copy for variable assignment and cleane unused code.

--- models/optimizers/FibonacciSearch.py
+++ models/optimizers/FibonacciSearch.py
@@ -3,51 +3,46 @@ import numpy as np
 from copy import copy
 
 class FibonacciSearch(optimizer):
-    def __init__(self, func, epsilon, maxIter = 1e18, interval = [-1e6, 1e6], xtol = 1e-6, ftol = 1e-6):
-         self.epsilon = epsilon
-         super().__init__(func, maxIter, interval, xtol, ftol)
-         self.fibArray = self._fibonacci(self.maxIter)
-
-
     def find_min(self):
+        self.fibArray = self._fibonacci(self.maxIter)
         self.I = self.interval[1] - self.interval[0]
         self.I = (self.fibArray[-2]/self.fibArray[-1])*self.I
         
         self.x_a = self.interval[1] - self.I
-        self.x_b = self.interval[0] - self.I
+        self.x_b = self.interval[0] + self.I
 
         self.fx_a = self.objectiveFunction(self.x_a)
         self.fx_b = self.objectiveFunction(self.x_b)
 
         for iteration in range(1, self.maxIter):
             self.I = self._get_intervalSize(iteration)
-            self._get_new_interval()
+            self._update_interval()
             if iteration == self.maxIter - 2:
                 break
             elif self.x_a > self.x_b:
                 break
-
+            elif (self.x_b - self.x_a) <= self.xtol:
+                break
         return self.x_a
 
     def _get_intervalSize(self, k):
-        old_I = self.I
+        old_I = copy(self.I)
         new_I = (self.fibArray[-2 - k]/self.fibArray[-1 - k]) * old_I
         return new_I
 
-    def _get_new_interval(self):
+    def _update_interval(self):
         if self.fx_a >= self.fx_b:
-            self.interval[0] = self.x_a
-            old_x_a = copy(self.x_a)
-            self.x_a = self.x_b
-            self.x_b = old_x_a + self.I
-            self.fx_a = self.fx_b
+            self.interval[0] = copy(self.x_a)
+            self.x_a = copy(self.x_b)
+            self.x_b = self.interval[0] + self.I
+            self.fx_a = copy(self.fx_b)
             self.fx_b = self.objectiveFunction(self.x_b)
         else:
-            self.interval[1] = self.x_b
-            old_x_a = self.x_a
+            self.interval[1] = copy(self.x_b)
+            old_x_a = copy(self.x_a)
             self.x_a = self.interval[1] - self.I
             self.x_b = old_x_a
-            self.fx_b = self.fx_a
+            self.fx_b = copy(self.fx_a)
             self.fx_a = self.objectiveFunction(self.x_a)
 
 
@@ -60,7 +55,7 @@ class FibonacciSearch(optimizer):
         for i in range(n):
             x_aux = x_new
             x_new = x_new + x_old
-            if x_new > 1e25:
+            if x_new > 1e30:
                 self.maxIter = i - 1
                 break
             fib_list += [x_new]
