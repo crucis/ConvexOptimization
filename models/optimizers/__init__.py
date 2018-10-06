8901048fc40fd5ab29c8678c185b0ee448c48aa3
Pedro Bandeira <bandeira85@gmail.com>
2018-10-05 23:15:55 -0300

    Added QIS to module.

--- models/optimizers/__init__.py
+++ models/optimizers/__init__.py
@@ -1,6 +1,7 @@
 from .DichotomousSearch import DichotomousSearch
 from .FibonacciSearch import FibonacciSearch
 from .GoldenSectionSearch import GoldenSectionSearch
+from .QuadraticInterpolationSearch import QuadraticInterpolationSearch
 from .optimizer import optimizer
 
-__all__ = ['DichotomousSearch', 'FibonacciSearch', 'GoldenSectionSearch', 'optimizer']
+__all__ = ['DichotomousSearch', 'FibonacciSearch', 'GoldenSectionSearch', 'QuadraticInterpolationSearch', 'optimizer']
