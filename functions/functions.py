44bcf84322effc347c45f56869aefb768bb5a141
bandeira <pedro.bandeira@greenant.com.br>
2018-10-08 13:47:44 -0300

    Added functions to be evaluated

---
+++ functions/functions.py
@@ -0,0 +1,19 @@
+import autograd.numpy as np
+
+
+
+def 5th_order_polynomial(x):
+    x = np.array(x)
+    return -5*x**5 + 4*x**4 - 12*x**3 + 11*x**2 - 2*x +1
+
+def logarithmic(x):
+    x = np.array(x)
+    return np.log(x-2)**2 + np.log(10-x)**2 - x**0.2
+
+def sinoid(x):
+    x = np.array(x)
+    return -3*x*np.sin(0.75*x) + np.exp(-2*x)
+
+def 4th_order_polynomial(x):
+    x = np.array(x)
+    return 0.7*x[0]**4 - 8*x[0]**2 + 6*x[1]**2 + np.cos(x[0]*x[1]) - 8*x[0]
