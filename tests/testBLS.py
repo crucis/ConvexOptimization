cfb018d00d39475d45e2404a4de64b826685cc17
bandeira <pedro.bandeira@greenant.com.br>
2018-10-09 17:41:39 -0300

    Created a test file for BacktrackingLineSearch

---
+++ tests/testBLS.py
@@ -0,0 +1,20 @@
+from functions import functionObj
+from models.optimizers import BacktrackingLineSearch
+import autograd.numpy as np
+
+f_x = lambda x: x**2 - 4*x + 4
+f_x_obj = functionObj(f_x)
+
+
+x_0 = np.random.randn(1)*100
+
+opt = BacktrackingLineSearch(f_x_obj,
+                            initial_x = x_0,
+                            delta_x = x_0*0.1,
+                            alpha = 0.01,
+                            beta = 0.5)
+
+
+x_min = opt.find_min()
+print('X: %.9f \nF_x: %.9f'%(x_min, f_x_obj(x_min)))
+print('Function evals: %d'%(f_x_obj.fevals - 1))
