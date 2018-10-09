1a16251595eff8d8ad7de2aecbaf3aac888b8043
Pedro Bandeira <bandeira85@gmail.com>
2018-10-08 22:20:35 -0300

    Renamed functions due to pep8

--- functions/functions.py
+++ functions/functions.py
@@ -2,7 +2,7 @@ import autograd.numpy as np
 
 
 
-def 5th_order_polynomial(x):
+def order5_polynomial(x):
     x = np.array(x)
     return -5*x**5 + 4*x**4 - 12*x**3 + 11*x**2 - 2*x +1
 
@@ -14,6 +14,6 @@ def sinoid(x):
     x = np.array(x)
     return -3*x*np.sin(0.75*x) + np.exp(-2*x)
 
-def 4th_order_polynomial(x):
+def order4_polynomial(x):
     x = np.array(x)
     return 0.7*x[0]**4 - 8*x[0]**2 + 6*x[1]**2 + np.cos(x[0]*x[1]) - 8*x[0]
