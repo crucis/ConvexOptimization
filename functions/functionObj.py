2957082de1a244cedfc0634665e44d7aa6da4611
Pedro Bandeira <bandeira85@gmail.com>
2018-10-09 00:00:30 -0300

    Added log functionalities for functionObj. self.all_evals, self.all_x

--- functions/functionObj.py
+++ functions/functionObj.py
@@ -1,15 +1,35 @@
 import autograd.numpy as np
+from copy import copy
 
 class functionObj:
     def __init__(self, func):    
         self.func = func
         self.fevals = 0
         self.best_x = np.inf
+        self.best_f = np.inf
+        self.all_evals = []
+        self.all_x = []
 
     def __call__(self, x):
         self.fevals = self.fevals + 1
         result = self.func(x)
-        assert np.isnan(result) == False, "X out of domain"
+        
+        # Autograd ArrayBox behaves differently from numpy, that fixes it.
+        if type(result) == np.numpy_boxes.ArrayBox:
+            result_copy = copy(result._value[0])
+        else:
+            result_copy = copy(result)
+        if type(x) == np.numpy_boxes.ArrayBox:
+            x_copy = x._value[0]
+        else:
+            x_copy = x
+        
+        assert np.isnan(result_copy) == False, "X out of domain"
+        self.all_evals += [result_copy]
+        self.all_x += [x_copy]
+        if result_copy < self.best_f:
+            self.best_x = x_copy
+            self.best_f = result_copy
         return result
 
     def reset_fevals(self):
