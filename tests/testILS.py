21348c472ac13e29b2b9088584e09306667dbfa7
Pedro Bandeira <bandeira85@gmail.com>
2018-10-08 23:23:02 -0300

    Created a test file for InexactLineSearch

---
+++ tests/testILS.py
@@ -0,0 +1,13 @@
+from functions import functionObj
+from models.optimizers import InexactLineSearch
+import autograd.numpy as np
+
+f_x = lambda x: x**2 - 4*x + 4
+f_x_obj = functionObj(f_x)
+
+x_0 = np.random.randn(1)*100
+opt = InexactLineSearch(f_x_obj)
+
+x_min = opt.find_min()
+print('X: %.9f \nF_x: %.9f'%(x_min, f_x_obj(x_min)))
+print('Function evals: %d'%(f_x_obj.fevals - 1))
