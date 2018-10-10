447f632001333fe6edf9187ecf91c3a8edc4880f
Pedro Bandeira de Mello Martins <bandeira85@estinge18.pads.ufrj.br>
2018-10-10 17:14:14 -0300

    Added plot

--- tests/testBLS.py
+++ tests/testBLS.py
@@ -1,16 +1,17 @@
 from functions import functionObj
 from models.optimizers import BacktrackingLineSearch
 import autograd.numpy as np
+import matplotlib.pyplot as plt
 
 f_x = lambda x: x**2 - 4*x + 4
 f_x_obj = functionObj(f_x)
 
+np.random.seed(42)
 
 x_0 = np.random.randn(1)*100
 
 opt = BacktrackingLineSearch(f_x_obj,
                             initial_x = x_0,
-                            delta_x = x_0*0.1,
                             alpha = 0.01,
                             beta = 0.5)
 
@@ -18,3 +19,7 @@ opt = BacktrackingLineSearch(f_x_obj,
 x_min = opt.find_min()
 print('X: %.9f \nF_x: %.9f'%(x_min, f_x_obj(x_min)))
 print('Function evals: %d'%(f_x_obj.fevals - 1))
+
+
+plt.plot(f_x_obj.all_evals)
+plt.show()
