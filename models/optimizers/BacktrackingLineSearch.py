2cd1c22dbf583009ce4928698b35bf1ac43fd81c
bandeira <pedro.bandeira@greenant.com.br>
2018-10-11 17:11:25 -0300

    Fixed BLS, by delta_x = -grad_fx

--- models/optimizers/BacktrackingLineSearch.py
+++ models/optimizers/BacktrackingLineSearch.py
@@ -1,15 +1,16 @@
 from .optimizer import optimizer
 from autograd import grad
 import autograd.numpy as np
+from copy import copy
 
 
 class BacktrackingLineSearch(optimizer):
     def __init__(self, func, 
                         initial_x,
-                        alpha = 0.01, 
-                        beta = 0.1,
+                        alpha = 0.3, 
+                        beta = 0.5,
                         interval = [-100, 100],
-                        maxIter = 1e3, 
+                        maxIter = 1e6, 
                         xtol = 1e-6, 
                         ftol = 1e-6):
         super().__init__(func = func, maxIter = maxIter, xtol = xtol, ftol = ftol, interval=interval)  
@@ -24,17 +25,17 @@ class BacktrackingLineSearch(optimizer):
 
     
     def find_min(self):
-        grad_x = self.grad_func(self.x)
+        grad_x = copy(self.grad_func(self.x))
         for _ in range(self.maxIter):
             self.t = 1
-            self.delta_x = grad_x
+            self.delta_x = copy(-grad_x)
             f_x = self.objectiveFunction(self.x)
-            while self.objectiveFunction(self.x + self.t*self.delta_x) \
-                >  f_x + self.alpha*self.t*np.transpose(grad_x)*self.delta_x:
+            f_x_tdeltax = self.objectiveFunction(self.x + self.t * self.delta_x)
+            while f_x_tdeltax  >  f_x + self.alpha*self.t*np.transpose(grad_x)*self.delta_x:
                 self.t = self.beta * self.t
-
+                f_x_tdeltax = self.objectiveFunction(self.x + self.t * self.delta_x)
             self.x = self.x + self.t * self.delta_x
-            grad_x = self.grad_func(self.x)
+            grad_x = copy(self.grad_func(self.x))
             if np.linalg.norm(grad_x) <= self.xtol:
                 break
         return self.x
