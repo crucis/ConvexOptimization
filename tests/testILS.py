7455ca25e68b3c89c8cb0302bb1ec3a0062b37d4
Pedro Bandeira <bandeira85@gmail.com>
2018-10-13 19:59:05 -0300

    InexactLineSearch doesn't need direction vector as input anymore

--- tests/testILS.py
+++ tests/testILS.py
@@ -8,8 +8,7 @@ f_x = lambda x: x**2 - 4*x + 4
 f_x_obj = functionObj(f_x)
 
 x_0 = np.random.randn(1)*100
-d_0 = copy(grad(f_x_obj)(x_0))
-opt = InexactLineSearch(f_x_obj, x_0, d_0)
+opt = InexactLineSearch(f_x_obj, x_0)
 
 x_min = opt.find_min()
 print('X: %.9f \nF_x: %.9f'%(x_min, f_x_obj(x_min)))
