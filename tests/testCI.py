dd96d220d52b10fae22b9124b9c56cd6a5195b2a
Pedro Bandeira <bandeira85@gmail.com>
2018-10-13 21:59:37 -0300

    Deleted unused line.

--- tests/testCI.py
+++ tests/testCI.py
@@ -7,7 +7,6 @@ f_x = lambda x: x**2 - 4*x + 4
 f_x_obj = functionObj(f_x)
 
 opt = CubicInterpolation(f_x_obj)
-#opt = CubicInterpolation(f_x_obj, np.random.randn(3), xtol = 1e-6, maxIter=2e10)
 
 x_min = opt.find_min()
 print('X: %.9f \nF_x: %.9f'%(x_min, f_x_obj(x_min)))
