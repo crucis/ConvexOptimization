22a02ab02de80714e2320f22dc014982ced90ceb
Pedro Bandeira <bandeira85@gmail.com>
2018-11-07 10:50:49 -0200

    Added function exercise520 and created a multiDim version of functionObj.

--- functions/__init__.py
+++ functions/__init__.py
@@ -3,12 +3,17 @@ from .functions import order5_polynomial
 from .functions import logarithmic
 from .functions import sinoid
 from .functions import exercise57
+from .functions import exercise520
 from .functionObj import functionObj
+from .functionObj import functionObj_multiDim
+
 
 
 __all__ = ['order4_polynomial',
             'order5_polynomial',
             'logarithmic',
             'sinoid',
-            'exercise57'
-            'functionObj']
+            'exercise57',
+            'exercise520',
+            'functionObj',
+            'functionObj_multiDim']
