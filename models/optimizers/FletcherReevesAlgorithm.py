20ea9a9cd49d31f81db3989cc578712d478a1a93
Pedro Bandeira <bandeira85@gmail.com>
2018-11-21 11:32:40 -0200

    Changed when algorithm is reinitialized.

--- models/optimizers/FletcherReevesAlgorithm.py
+++ models/optimizers/FletcherReevesAlgorithm.py
@@ -1,16 +1,17 @@
 from .optimizer import optimizer
 import autograd.numpy as np
+#from scipy import optimize
 
 def _fletcher_reeves_algorithm(x, objectiveFunction, line_search, xtol=1e-6, maxIter=1e3):
     for _ in range(maxIter):
         grad_f = objectiveFunction.grad(x)
         direction_vector = -grad_f
-        for i in range(100):
+        for i in range(1000):
             alpha, _ = line_search(x, direction_vector)
             x = x + alpha * direction_vector
             if np.linalg.norm(alpha * direction_vector) < xtol:
                 return x
-            if i == 10:
+            if i == 100:
                 break
             grad_f_new = objectiveFunction.grad(x)
             beta = np.dot(grad_f_new.T, grad_f_new)/np.dot(grad_f.T, grad_f)
