a69c281459cc29e8c59a8343283cc0d698771b83
Pedro Bandeira de Mello Martins <bandeira85@estinge15.pads.ufrj.br>
2018-11-23 21:31:43 -0200

    Updated testFib to new FibonacciSearch

--- tests/testFib.py
+++ tests/testFib.py
@@ -7,6 +7,6 @@ f_x_obj = functionObj(f_x)
 opt = FibonacciSearch(f_x_obj, xtol = 1e-6, maxIter=2e10)
 
 
-x_min = opt.find_min()
+x_min, _ = opt._line_search()
 print('X: %.9f \nF_x: %.9f'%(x_min, f_x_obj(x_min)))
 print('Function evals: %d'%(f_x_obj.fevals - 1))
