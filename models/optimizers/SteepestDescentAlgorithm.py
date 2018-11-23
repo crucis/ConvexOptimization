68812020e1b1d81d92c4846bed9daadac1e441dd
Pedro Bandeira de Mello Martins <bandeira85@estinge15.pads.ufrj.br>
2018-11-23 21:30:24 -0200

    Simplified the algorithm.

--- models/optimizers/SteepestDescentAlgorithm.py
+++ models/optimizers/SteepestDescentAlgorithm.py
@@ -5,25 +5,24 @@ from copy import copy
 
 def _steepest_descent_algorithm(x_0, objectiveFunction, maxIter, line_search=None, xtol=1e-6):
     x_k = np.array(x_0, dtype=np.float64)
-    grad_x = copy(objectiveFunction.grad(x_k))
-    direction_vector = copy(-grad_x)
     if line_search is None:
         a0 = 1
         f0 = objectiveFunction(x_k)
     for _ in range(maxIter):
+        grad_x = objectiveFunction.grad(x_k)
+        direction_vector = copy(-grad_x)
+       
         if line_search is not None:
-            a0, f0  = line_search(x_k, direction_vector)
+            a0, _  = line_search(x_k, direction_vector)
         else:
             f_hat = objectiveFunction(x_k - a0*grad_x)
-            a0 = (np.dot(grad_x, grad_x) * (a0 **2)) / (2 * (f_hat - f0 + a0 * np.dot(grad_x, grad_x)))
+            a0 = (np.dot(grad_x.T, grad_x) * (a0 **2)) / (2 * (f_hat - f0 + a0 * np.dot(grad_x.T, grad_x)))
         x_k = x_k + a0* direction_vector
         if line_search is None:
             f0 = objectiveFunction(x_k)
-        grad_x = copy(objectiveFunction.grad(x_k))
-        if np.linalg.norm(a0 * direction_vector) < xtol:
+
+        if np.linalg.norm(a0 * direction_vector, ord=2) < xtol:
             break
-        direction_vector = copy(-grad_x)
-        #direction_vector = copy(-grad_x/np.linalg.norm(grad_x))
     return x_k, a0
 
 class SteepestDescentAlgorithm(optimizer):
