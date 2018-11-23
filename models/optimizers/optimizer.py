3b908b11f3bfe715830f13eacc86fcd0fed30819
Pedro Bandeira de Mello Martins <bandeira85@estinge15.pads.ufrj.br>
2018-11-23 21:43:03 -0200

    Added a new parameter x_0

--- models/optimizers/optimizer.py
+++ models/optimizers/optimizer.py
@@ -1,10 +1,11 @@
 class optimizer:
-    def __init__(self, func, maxIter = 1e6, interval = [-1e6, 1e6], xtol = 1e-6, ftol = 1e-6):
+    def __init__(self, func, x_0=None, maxIter = 1e6, interval = [-5, 5], xtol = 1e-6, ftol = 1e-6):
         self.objectiveFunction = func
         self.maxIter = int(maxIter)
         self.interval = interval
         self.xtol = xtol
         self.ftol = ftol
-    
+        self.x_0 = x_0
+
     def find_min(self):
         pass
