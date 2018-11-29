ac61d9ff501db57d249d77b4d4531d656e789044
Pedro Bandeira <bandeira85@gmail.com>
2018-11-29 00:37:57 -0200

    Created a UnconstrainProblem class, to solve constrained problems.

---
+++ models/utils/unconstrain.py
@@ -0,0 +1,37 @@
+from autograd import numpy as np
+
+
+class UnconstrainProblem:
+    def __init__(self, f, x0, opt, mu=20, line_search_optimizer=None, maxIter = 1e3, xtol=1e-6):
+        self.objectiveFunction = f
+        self.x_0 = x0
+        self.optimizer = opt
+        self.maxIter = maxIter
+        self.xtol = xtol
+        self.mu = mu
+        self.line_search = line_search_optimizer
+        self.params = { 'xtol': self.xtol,
+                        'maxIter': self.maxIter,
+                        }
+        if self.line_search is not None:
+            self.params['line_search_optimizer'] = self.line_search
+
+    def find_min(self):
+        self.objectiveFunction.smooth_log_constant = 1
+        x0 = self.x_0
+        for _ in range(int(self.maxIter)):
+            # centering Step
+            opt = self.optimizer(self.objectiveFunction, x0, **self.params)
+            opt.find_min()
+        
+            # Update x_0
+            if self.objectiveFunction._has_eqc:
+                x0 = self.objectiveFunction.best_z
+            else:
+                x0 = self.objectiveFunction.best_x
+            # Break if m/t < epsilon
+            if self.objectiveFunction.niq/self.objectiveFunction.smooth_log_constant < self.xtol:
+                break
+            # Update t
+            self.objectiveFunction.smooth_log_constant *= self.mu
+        return self.objectiveFunction.best_x
