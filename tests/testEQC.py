285f7d1067a28fb002ebf533acd289674d0dda1a
Pedro Bandeira <bandeira85@gmail.com>
2018-11-29 00:38:34 -0200

    Finished testEQC to test also Inequaly constraints.

--- tests/testEQC.py
+++ tests/testEQC.py
@@ -1,4 +1,5 @@
 from models.optimizers import SteepestDescentAlgorithm, BacktrackingLineSearch
+from models.utils import UnconstrainProblem
 from functions import functionObj
 from scipy import optimize
 from autograd import numpy as np
@@ -6,6 +7,7 @@ from autograd import numpy as np
 A = np.array([[1,2,1,2], [1,1,2,4]], dtype=np.float64)
 b = np.array([[3], [5]], dtype=np.float64)
 c = np.array([1, 1.5, 1, 1], dtype=np.float64)
+bound = [lambda x: -x[0],lambda x:  -x[1],lambda x:  -x[2],lambda x:  -x[3]]
 f_x = lambda x: np.dot(c, x)
 print('----------ScipySimplex----------')
 
@@ -14,12 +16,11 @@ print(res)
 
 print('----------QuasiNewton----------')
 f_x_obj = functionObj(f_x)
-f_x_obj.add_constraints((A, b))
-x_0 = np.array([-1,-1], dtype=np.float64)
+f_x_obj.add_constraints(equality=(A, b), inequality=bound)
+x_0 = np.array([0, 0], dtype=np.float64)
 line_search = BacktrackingLineSearch(f_x_obj, x_0)
 
-SteepestDescentAlgorithm(f_x_obj, x_0, line_search_optimizer=line_search).find_min()
-x0 = np.dot(f_x_obj._null_space_feasible_matrix, x_0[:, np.newaxis]) + f_x_obj._feasible_vector
+UnconstrainProblem(f_x_obj, x_0, SteepestDescentAlgorithm, line_search_optimizer=line_search).find_min()
 x_min = np.array(f_x_obj.best_x)
 print('x: ', x_min)
 f_min = f_x_obj.best_f
