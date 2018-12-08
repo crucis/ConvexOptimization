18b6fc83ea4d26d100e299ec4fb4b01f63933c4d
Pedro Bandeira <bandeira85@gmail.com>
2018-12-08 03:06:16 -0200

    Tolerance now updates at each update of smooth_log_constant. Line search updated as well.

--- models/utils/unconstrain.py
+++ models/utils/unconstrain.py
@@ -2,7 +2,7 @@ from autograd import numpy as np
 
 
 class UnconstrainProblem:
-    def __init__(self, func, x_0, opt, mu=20, line_search_optimizer=None, maxIter = 1e3, xtol=1e-6):
+    def __init__(self, func, x_0, opt, mu=20, line_search_optimizer=None, maxIter = 1e3, xtol=1e-10):
         self.objectiveFunction = func
         self.x_0 = x_0
         self.optimizer = opt
@@ -13,14 +13,17 @@ class UnconstrainProblem:
         self.params = { 'xtol': self.xtol,
                         'maxIter': self.maxIter,
                         }
-        if self.line_search is not None:
-            self.params['line_search_optimizer'] = self.line_search
 
     def find_min(self):
-        self.objectiveFunction.smooth_log_constant = 1
+        self.objectiveFunction.smooth_log_constant = 1000
         x0 = self.x_0
         for _ in range(int(self.maxIter)):
-            self.params['xtol'] = self.objectiveFunction.niq/self.objectiveFunction.smooth_log_constant
+            self.params['xtol'] = self.objectiveFunction.niq/(self.objectiveFunction.smooth_log_constant)
+            if self.line_search is not None:
+                ls_params = {'xtol': self.params['xtol'],
+                            'maxIter': self.params['maxIter']}
+                ls = self.line_search(self.objectiveFunction, x0, **ls_params)
+                self.params['line_search_optimizer'] = ls
             # centering Step
             opt = self.optimizer(self.objectiveFunction, x0, **self.params)
             opt.find_min()
